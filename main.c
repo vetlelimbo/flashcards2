10bcf800439836ddd5e61d6f7fead45c8a9c3440
vetlelimbo <vetle.werner03@gmail.com>
2026-05-03 20:06:11 +0200

    Create the initial structs for flashcard and main data structure.

--- main.c
+++ main.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+typedef struct Flashcard {
+	char question[100];
+	char answer[100];
+} flashcard;
+
+typedef struct All_Flashcards {
+	int top_of_deck;
+	flashcard cards[10];
+} all_flashcards;
 
 int main() {
-	puts("This is just a test");
-	return 0;
+	all_flashcards flashcards = {0};
+	return EXIT_SUCCESS;
 }
